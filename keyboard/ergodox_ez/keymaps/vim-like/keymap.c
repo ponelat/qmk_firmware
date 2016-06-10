7dd9d25e7b9f42c0bca9e66839ad8435a5123991
Josh Ponelat <josh@ponelat.com>
2016-06-10 08:33:04 +0200

    change lshft and rshft to be tap minus and tap equals

--- keyboard/ergodox_ez/keymaps/vim-like/keymap.c
+++ keyboard/ergodox_ez/keymaps/vim-like/keymap.c
@@ -6,8 +6,8 @@
 #define SYMB 1 // symbols
 #define MDIA 2 // media keys
 
-#define LSFTO M(0) // Left shift, open parens when tapped
-#define RSFTC M(1) // Right shift, close parens when tapped
+/* #define LSFTO M(0) // Left shift, open parens when tapped */
+/* #define RSFTC M(1) // Right shift, close parens when tapped */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_EQL,           KC_1,           KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
         ALT_T(KC_GRV),    KC_Q,           KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         CTL_T(KC_ESC),    KC_A,           KC_S,   KC_D,   KC_F,   KC_G,
-        LSFTO,            KC_Z,           KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
+        SFT_T(KC_MINS),   KC_Z,           KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
         MO(SYMB),         KC_QUOT,        LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
                                                ALT_T(KC_APP),     KC_LGUI,
                                                                   KC_HOME,
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_RGHT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
              TG(SYMB),    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             ALT_T(KC_BSLS),
                           KC_H,   KC_J,   KC_K,   KC_L,   LT(SYMB, KC_SCLN),CTL_T(KC_QUOT),
-             MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          RSFTC,
+             MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          SFT_T(KC_EQL),
                                   KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,          LT(MDIA,KC_PLUS),
              KC_LALT,        CTL_T(KC_ESC),
              KC_PGUP,
@@ -147,34 +147,34 @@ static uint16_t key_timer;
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
-      switch(id) {
-        case 0: {
-            if (record->event.pressed) {
-                key_timer = timer_read(); // if the key is being pressed, we start the timer.
-                register_code(KC_LSFT); // we're now holding down Shift.
-            } else { // this means the key was just released, so we can figure out how long it was pressed for (tap or "held down").
-                if (timer_elapsed(key_timer) < 120) { // the threshhold we pick for counting something as a tap. 
-                    register_code(KC_9); // sending 9 while Shift is held down gives us an opening paren
-                    unregister_code(KC_9); // now let's let go of that key
-                }
-                unregister_code(KC_LSFT); // let's release the Shift key now.
-            }
-            break;
-        }
-        case 1: {
-            if (record->event.pressed) {
-                key_timer = timer_read(); // Now we're doing the same thing, only for the right shift/close paren key
-                register_code(KC_RSFT); 
-            } else { 
-                if (timer_elapsed(key_timer) < 120) { 
-                    register_code(KC_0); 
-                    unregister_code(KC_0); 
-                }
-                unregister_code(KC_RSFT); 
-            }
-            break;
-        }
-      }
+      /* switch(id) { */
+      /*   case 0: { */
+      /*       if (record->event.pressed) { */
+      /*           key_timer = timer_read(); // if the key is being pressed, we start the timer. */
+      /*           register_code(KC_LSFT); // we're now holding down Shift. */
+      /*       } else { // this means the key was just released, so we can figure out how long it was pressed for (tap or "held down"). */
+      /*           if (timer_elapsed(key_timer) < 120) { // the threshhold we pick for counting something as a tap. */ 
+      /*               register_code(KC_9); // sending 9 while Shift is held down gives us an opening paren */
+      /*               unregister_code(KC_9); // now let's let go of that key */
+      /*           } */
+      /*           unregister_code(KC_LSFT); // let's release the Shift key now. */
+      /*       } */
+      /*       break; */
+      /*   } */
+      /*   case 1: { */
+      /*       if (record->event.pressed) { */
+      /*           key_timer = timer_read(); // Now we're doing the same thing, only for the right shift/close paren key */
+      /*           register_code(KC_RSFT); */ 
+      /*       } else { */ 
+      /*           if (timer_elapsed(key_timer) < 120) { */ 
+      /*               register_code(KC_0); */ 
+      /*               unregister_code(KC_0); */ 
+      /*           } */
+      /*           unregister_code(KC_RSFT); */ 
+      /*       } */
+      /*       break; */
+      /*   } */
+      /* } */
     return MACRO_NONE;
 };
 
